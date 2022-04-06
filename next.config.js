927166a7a76d69c8131aeeabe8e3d256b180e115
Daniel Zaleta <zaletadaniel@gmail.com>
2022-04-06 18:38:59 -0500

    eslint, prettier, next.config, jsconfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    customKey: 'customValue',
+  },
+  basePath: '/dist',
+  compress: true,
+  async redirects(){
+    return [
+      {
+        source: '/hola',
+        destination: 'https://www.linkedin.com/in/danzaleta/',
+        permanent: true,
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
