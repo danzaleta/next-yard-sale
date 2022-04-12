97773e6f83be3575d1fbdd4b9b89176f92255de9
Daniel Zaleta <zaletadaniel@gmail.com>
2022-04-12 13:32:31 -0500

    Google analytics config

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,22 @@
 import Header from '@components/Header';
+import Script from 'next/script';
 import AppContext from '@context/AppContext';
 import useInitialState from '@hooks/useInitialState';
 import '@styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   const initialState = useInitialState();
-
   return (
     <AppContext.Provider value={initialState}>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-M93BFX2EWJ" strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-M93BFX2EWJ');
+        `}
+      </Script>
       <Header />
       <Component {...pageProps} />
     </AppContext.Provider>
