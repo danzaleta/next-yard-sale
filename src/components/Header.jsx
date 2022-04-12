febcb95f380ab323ea4f632157ee51a6e8f20fa0
Daniel Zaleta <zaletadaniel@gmail.com>
2022-04-12 12:52:09 -0500

    Styles fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
 
   return (
     <nav className={styles.Nav}>
-      <Image src={menu} alt="menu" className={styles.menu} />
+      <img src={menu.src} alt="menu" className={styles.menu} />
       <div className={styles['navbar-left']}>
         <Link href="/" passHref>
           <Image src={logo} alt="logo" className={styles['menu-logo']} />
